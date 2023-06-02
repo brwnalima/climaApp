1b39c44f1d76749b8abd476cfb4110f40c93adeb
brwnalima <bruna.lima@edu.unirio.br>
2023-06-02 18:24:43 -0300

    components

---
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import axios from 'axios';
+
+
+
+function CurrentWeather() {
+
+    async function fetchWeatherData() {
+        const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+        const city = 'Rio de Janeiro';
+
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+            );
+
+            console.log(response.data);
+            // Aqui você pode tratar os dados da resposta como desejar
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData();
+    }, []);
+
+    return (
+        <div>CurrentWeather</div>
+    )
+}
+
+export default CurrentWeather
---
+++ src/components/pages/weatherPage/WeatherPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CurrentWeather from '../../currentWeather/CurrentWeather'
+
+function WeatherPage() {
+    return (
+        <div>
+            WeatherPage
+            <CurrentWeather />
+        </div>
+
+    )
+}
+
+export default WeatherPage
