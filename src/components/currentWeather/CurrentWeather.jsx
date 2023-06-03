225b937816f8c44cf8be450af323ed45197c2811
brenddamachado <brenda16machado.bm@gmail.com>
2023-06-03 00:43:42 -0300

    pesquisa de clima

--- src/components/currentWeather/CurrentWeather.jsx
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -1,33 +1,90 @@
-import React, { useEffect } from 'react'
 import axios from 'axios';
-
-
+import React, { useEffect, useState } from 'react';
 
 function CurrentWeather() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [translatedDescription, setTranslatedDescription] = useState('');
 
-    async function fetchWeatherData() {
-        const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
-        const city = 'Rio de Janeiro';
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
 
-        try {
-            const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-            );
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData();
+  };
 
-            console.log(response.data);
-            // Aqui você pode tratar os dados da resposta como desejar
-        } catch (error) {
-            console.error(error);
-        }
+  async function fetchWeatherData() {
+    const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+
+    try {
+      setIsLoading(true);
+
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+
+      if (response.status === 200) {
+        setWeatherData(response.data);
+        translateDescription(response.data.weather[0].description);
+      } else {
+        console.log('Não foi possível obter o clima.');
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    useEffect(() => {
-        fetchWeatherData();
-    }, []);
+  async function translateDescription(description) {
+    try {
+      const response = await axios.get(
+        'https://translate.googleapis.com/translate_a/single',
+        {
+          params: {
+            client: 'gtx',
+            sl: 'en',
+            tl: 'pt',
+            dt: 't',
+            q: description,
+          },
+        }
+      );
 
-    return (
-        <div>CurrentWeather</div>
-    )
+      const translatedText = response.data[0][0][0];
+      setTranslatedDescription(translatedText);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Digite o nome da cidade"
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      {isLoading ? (
+        <p>Carregando...</p>
+      ) : weatherData ? (
+        <div>
+          <h1> {weatherData.name}</h1>
+          <p>Descrição: {translatedDescription}</p>
+          <p>Temperatura: {weatherData.main.temp.toFixed(0)}°C</p>
+          <p>Umidade: {weatherData.main.humidity}%</p>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export default CurrentWeather
+export default CurrentWeather;
