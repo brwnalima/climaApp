f6e430cb9fd8948ce8052b245bf1b88953afd973
brwnalima <bruna.lima@edu.unirio.br>
2023-06-03 01:14:04 -0300

    css

--- src/components/currentWeather/CurrentWeather.jsx
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -5,25 +5,25 @@ import axios from 'axios';
 
 function CurrentWeather() {
 
-    async function fetchWeatherData() {
-        const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
-        const city = 'Rio de Janeiro';
+    // async function fetchWeatherData() {
+    //     const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+    //     const city = 'São Paulo';
 
-        try {
-            const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-            );
+    //     try {
+    //         const response = await axios.get(
+    //             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    //         );
 
-            console.log(response.data);
-            // Aqui você pode tratar os dados da resposta como desejar
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    //         console.log(response.data);
+    //         // Aqui você pode tratar os dados da resposta como desejar
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
 
-    useEffect(() => {
-        fetchWeatherData();
-    }, []);
+    // useEffect(() => {
+    //     fetchWeatherData();
+    // }, []);
 
     return (
         <div>CurrentWeather</div>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,46 @@
-import {React, useState} from 'react'
+import { React, useState, useEffect } from 'react'
 import './SearchBar.css'
 import { FiSearch } from 'react-icons/fi';
-
+import axios from 'axios';
 
 function SearchBar() {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState({});
+
+
+  useEffect(() => {
+    async function fetchWeatherData() {
+      const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm ? searchTerm : "Rio de Janeiro"}&units=metric&appid=${apiKey}`;
+  
+  
+      try {
+        const response = await axios.get(apiUrl);
+        setData(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+  }, []);
+  
+  async function fetchWeatherData() {
+    const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm ? searchTerm : "Rio de Janeiro"}&units=metric&appid=${apiKey}`;
+
+
+    try {
+      const response = await axios.get(apiUrl);
+      setData(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -13,22 +48,30 @@ function SearchBar() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Lógica para lidar com a submissão da pesquisa
-    console.log('Termo de pesquisa:', searchTerm);
+    fetchWeatherData();
     setSearchTerm('');
   };
 
-
   return (
-    <form className='search-bar' onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleChange}
-        placeholder="Digite sua pesquisa"
-      />
-      <button type="submit"><FiSearch/></button>
-    </form>
+    <div>
+      <form className='search-bar' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Digite sua pesquisa"
+        />
+        <button type="submit"><FiSearch /></button>
+      </form>
+
+      {Object.keys(data).length !== 0 && (
+        <div>
+          <p>{data.name}</p>
+          <p>{data.main.temp}</p>
+        </div>
+      )}
+
+    </div>
   )
 }
 
