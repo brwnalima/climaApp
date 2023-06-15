14971e082aa79ee43cc0833a2286e76305f5b01d
brwnalima <bruna.lima@edu.unirio.br>
2023-06-15 14:01:25 -0300

    adding

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -50,40 +50,90 @@ function SearchBar() {
     }
   }
 
+  const fetchWeatherForecast = async () => {
+    try {
+      const apiKey = '944c94361980e1ab31ac912b9aaa3e73';
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city === "" ? "Rio de Janeiro" : city}&appid=${apiKey}&units=metric`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // Group forecast by day
+      const dailyForecast = {};
+      data.list.forEach((item) => {
+        const date = item.dt_txt.split(' ')[0];
+        if (!dailyForecast[date]) {
+          dailyForecast[date] = item;
+        } else {
+          if (item.dt_txt.split(' ')[1] === '12:00:00') {
+            dailyForecast[date] = item;
+          }
+        }
+      });
+
+      setForecast(Object.values(dailyForecast));
+      setCity(data.city.name);
+    } catch (error) {
+      console.error('Error fetching weather forecast:', error);
+    }
+  };
+
+  const translateDescription = (description) => {
+    const translationMap = {
+      Clear: 'Céu Limpo',
+      Clouds: 'Nublado',
+      Rain: 'Chuva',
+      Drizzle: 'Garoa',
+      Thunderstorm: 'Tempestade',
+      Snow: 'Neve',
+      Mist: 'Neblina',
+      Smoke: 'Fumaça',
+      Haze: 'Nevoeiro',
+      Dust: 'Poeira',
+      Fog: 'Névoa',
+      Sand: 'Areia',
+      Ash: 'Cinzas',
+      Squall: 'Squall',
+      Tornado: 'Tornado',
+    };
+
+    return translationMap[description] || description;
+  };
+
   function getWeatherIcon(description) {
     switch (description) {
       case 'clear sky':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-sunny-sun-6263771-5122307.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-sunny-sun-6263771-5122307.png?f=webp' />;
       case 'few clouds':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-day-8323005-6634090.png'/>;
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-day-8323005-6634090.png' />;
       case 'scattered clouds':
-        return <img src='https://static.vecteezy.com/system/resources/previews/019/061/827/original/cloud-3d-icon-png.png'/>; 
+        return <img src='https://static.vecteezy.com/system/resources/previews/019/061/827/original/cloud-3d-icon-png.png' />;
       case 'broken clouds':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloud-7082891-5740866.png'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloud-7082891-5740866.png' />;
       case 'overcast clouds':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-4599465-3803105.png?f=webp'/>;  
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-4599465-3803105.png?f=webp' />;
       case 'light rain':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/raining-cloud-6372435-5272553.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/raining-cloud-6372435-5272553.png?f=webp' />;
       case 'moderate rain':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/rain-5344468-5529275.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/rain-5344468-5529275.png?f=webp' />;
       case 'heavy rain':
-        return <img src='https://static.vecteezy.com/system/resources/previews/008/854/791/original/thunderstorm-rain-icon-weather-forecast-meteorological-sign-3d-render-png.png'/>; 
+        return <img src='https://static.vecteezy.com/system/resources/previews/008/854/791/original/thunderstorm-rain-icon-weather-forecast-meteorological-sign-3d-render-png.png' />;
       case 'light snow':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-with-snowflakes-6372437-5272555.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-with-snowflakes-6372437-5272555.png?f=webp' />;
       case 'moderate snow':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-with-snowflakes-6372437-5272555.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/clouds-with-snowflakes-6372437-5272555.png?f=webp' />;
       case 'heavy snow':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-snow-rain-6624403-5526277.png'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-snow-rain-6624403-5526277.png' />;
       case 'thunderstorm':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-snow-rain-6624403-5526277.png'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/cloudy-snow-rain-6624403-5526277.png' />;
       case 'mist':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/foggy-4713694-3918074.png'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/foggy-4713694-3918074.png' />;
       case 'fog':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/windy-snow-day-7812731-6267489.png'/>;  
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/windy-snow-day-7812731-6267489.png' />;
       case 'haze':
-        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/windy-lightining-snow-day-7812725-6267483.png?f=webp'/>; 
+        return <img src='https://cdn3d.iconscout.com/3d/premium/thumb/windy-lightining-snow-day-7812725-6267483.png?f=webp' />;
       default:
-        return 'unknown-icon'; 
+        return 'unknown-icon';
     }
   }
 
