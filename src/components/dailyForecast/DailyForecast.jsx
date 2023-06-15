d7de5e6a149d3a4a4d3749c2565315b5989b9aab
brwnalima <bruna.lima@edu.unirio.br>
2023-06-15 14:00:55 -0300

    adding

--- src/components/dailyForecast/DailyForecast.jsx
+++ src/components/dailyForecast/DailyForecast.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { format } from 'date-fns';
 
-function DailyForecast() {
+function DailyForecast({ forecast, translateDescription }) {
+
   return (
-    <div>DailyForecast</div>
-  )
+    <div>
+      {forecast ? (
+        forecast.map((item) => (
+          <div key={item.dt}>
+            <p>Date: {format(new Date(item.dt_txt), 'dd/MM')}</p>
+            <p>Temperature: {item.main.temp.toFixed(0)} °C</p>
+            <p>Description: {translateDescription(item.weather[0].main)}</p>
+          </div>
+        ))
+      ) : null }
+    </div>
+  );
 }
 
-export default DailyForecast
+export default DailyForecast;
